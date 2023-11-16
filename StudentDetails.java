b220088ffbc7eb1287f1dcea4d717fd48d19fcf8
unknown <tiatieno956@gmail.com>
2023-11-17 02:13:31 +0300

    first commit

---
+++ StudentDetails.java
@@ -0,0 +1,11 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        // Printing information about the student
+        System.out.println("Course: DIT");
+        System.out.println("Name: SUSAN ATIENO");
+        System.out.println("Registration number: 22/08482");
+        System.out.println("Date of birth: 24/10/2002");
+        System.out.println("Vision: Sky is the limit");
+        System.out.println("Favorite unit: Java programming");
+    }
+}
